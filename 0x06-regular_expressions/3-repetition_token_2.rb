6588cdf13c3dd1cd4d542bce1d93b4003e473b44
johnudoudo <mercausafetygroupng@gmail.com>
2023-05-04 03:44:16 -0700

    done

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#uby script that accepts one argument
+puts ARGV[0].scan(/hbt+n/).join
---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#a Ruby script that accepts one argument
+puts ARGV[0].scan(/^h.n$/).join
